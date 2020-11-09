e94373db726bd30d3f3190dee7f133a9349cc576
Jeremy Dombrowski <dev@jeremyfelix.com>
2020-11-09 14:23:42 -0800

    Hello route

--- express_server.js
+++ express_server.js
@@ -15,6 +15,10 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get('/hello', (req, res) => {
+  res.send('<html><body><h1>Hello World</h1></body></html>');
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
