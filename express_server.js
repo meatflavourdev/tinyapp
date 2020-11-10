b7f54484547300776f98826ea792e16086da54dd
Jeremy Dombrowski <dev@jeremyfelix.com>
2020-11-09 20:22:26 -0800

    url route display from data

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 3000; // default port 8080
 
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -15,17 +17,9 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body><h1>Hello World</h1></body></html>');
-});
-
-app.get('/set', (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
-
-app.get('/fetch', (req, res) => {
-  res.send(`a = ${a}`);
+app.get('/urls', (req, res) => {
+  const templateVars = { urls : urlDatabase };
+  res.render('urls_index', templateVars);
 });
 
 app.listen(PORT, () => {
