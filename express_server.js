cdfb8f1e1e2bbbba7ecafd9692a89a2f958efc40
Jeremy Dombrowski <dev@jeremyfelix.com>
2020-11-09 22:32:33 -0800

    Implement post url route and add longURL to urlDatabase variable

--- express_server.js
+++ express_server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 3000; // default port 8080
 
+// Body Parser Middleware - Converts request body from Buffer to String
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
@@ -22,9 +26,20 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.post('/urls', (req, res) => {
+  console.log(`longURL: ${req.body.longURL}`);
+  const { generateRandomString } = require('./shortIDs');
+  urlDatabase[generateRandomString()] = req.body.longURL;
+  res.send('OK');
+});
+
+app.get('/urls/new', (req, res) => {
+  const templateVars = { urls : urlDatabase };
+  res.render('urls_new', templateVars);
+});
+
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render('urls_show', templateVars);
+  res.render('urls_show');
 });
 
 app.listen(PORT, () => {
